281f98334b30f3e404864e5ea296de3360919556
emovid <misbach.imaduddin@yahoo.com>
2016-12-08 15:10:13 +0700

    Menambahkan halaman terimakasih setelah mengirim email

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,170 @@
+ <html>
+
+    <head>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+        <meta charset="utf-8">
+
+        <!-- Description, Keywords and Author -->
+
+        <meta name="description" content="">
+
+        <meta name="author" content="Misbach Imaduddin Zuhdi">
+
+        <title>Misbach Imaduddin</title>
+
+        <link href="{{ URL::asset('mi-icon.ico') }}" rel="shortcut icon" />  
+        
+
+        <!-- style -->
+
+        <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet" type="text/css">
+
+        <!-- style -->
+
+        <!-- bootstrap -->
+
+        <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+
+        <!-- responsive -->
+
+        <link href="{{ URL::asset('css/responsive.css') }}" rel="stylesheet" type="text/css">
+
+        <!-- font-awesome -->
+
+        <link href="{{ URL::asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+
+        <!-- font-awesome -->
+
+        <link href="{{ URL::asset('css/effects/set2.css') }}" rel="stylesheet" type="text/css">
+
+        <link href="{{ URL::asset('css/effects/normalize.css') }}" rel="stylesheet" type="text/css">
+
+        <link href="{{ URL::asset('css/effects/component.css') }}"  rel="stylesheet" type="text/css" >
+      
+        <!-- button style -->
+        <style>
+        .button {
+            background-color: #fb5353;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 20px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+        .button:hover {
+          background-color: #ef0e0e;
+          color: #ffffff;
+          border-color: #fc3c3c;
+        }
+        </style>
+        <!-- button style -->
+
+    </head>
+
+
+
+    <body>
+
+
+
+        <!-- main -->
+<main role="main-inner-wrapper" class="container">
+
+
+
+    <div class="row row-centered">
+
+
+
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center ">
+
+            <article role="pge-title-content" class="contact-header">
+
+                <header>
+
+                    <h2><span>Thank You!</span></h2>
+
+                </header>
+
+                
+                
+                <img src="images/thankyou-images/envelope-2.png" alt="" class="img-responsive center-block"/>
+                </br>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </br>
+                <div class="col-xs-2"></div>
+                <div class="col-xs-8"><a href="{{ url('/') }}" class="button btn-block ">Continue</a></div>
+                <div class="col-xs-2"></div>
+                
+
+            </article>
+
+        </div>
+
+       <!-- contat-from-wrapper -->
+
+
+        </div>
+
+
+        <div class="clearfix"></div>
+
+
+
+       <!-- contat-from-wrapper -->
+
+       
+
+
+
+
+
+    </div>
+
+</main>
+<!-- main -->
+
+
+       
+
+    
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+        <script src="{{ URL::asset('js/jquery.min.js') }}" type="text/javascript"></script>
+
+        <!-- custom -->
+
+        <script src="{{ URL::asset('js/nav.js') }}" type="text/javascript"></script>
+
+        <script src="{{ URL::asset('js/custom.js') }}" type="text/javascript"></script>
+
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+        <script src="{{ URL::asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
+
+        <script src="{{ URL::asset('js/effects/masonry.pkgd.min.js') }}"  type="text/javascript"></script>
+
+        <script src="{{ URL::asset('js/effects/imagesloaded.js') }}"  type="text/javascript"></script>
+
+        <script src="{{ URL::asset('js/effects/classie.js') }}"  type="text/javascript"></script>
+
+        <script src="{{ URL::asset('js/effects/AnimOnScroll.js') }}"  type="text/javascript"></script>
+
+        <script src="{{ URL::asset('js/effects/modernizr.custom.js') }}"></script>
+
+        <!-- jquery.countdown -->
+
+        <script src="{{ URL::asset('js/html5shiv.js') }}" type="text/javascript"></script>
+        
+
+    </body>
+
+</html>
